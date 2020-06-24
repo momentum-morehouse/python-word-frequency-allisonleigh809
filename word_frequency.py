08155fa21bf1b90b091f29f9cc56ed1712a15717
allisonleigh808 <allisonleigh808@gmail.com>
2020-06-24 23:09:00 +0000

    did not finish but tried did part of the word count and part of the stop word

--- word_frequency.py
+++ word_frequency.py
@@ -3,16 +3,40 @@ STOP_WORDS = [
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
     'will', 'with'
 ]
+STOP_WORDS.remove(('a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
+    'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
+    'will', 'with'))
+
+def flatten_lol(lol):
+  flat_list = []
+  for sublist in lol:
+    for item in sublist:
+      flat_list.append(item)
+  return flat_list
+
 
 
 def print_word_freq(file):
-    """Read in `file` and print out the frequency of words in that file."""
-    # Your code will go here. You can write additional functions if you want, and call them inside this function.
-    # first open the file
-    with open(file) as f:
+  """Read in `file` and print out the frequency of words in that file."""
+  # Your code will go here. You can write additional
+  #  functions if you want, and call them inside this function.
+  # first open the file
+  with open(file) as f:
         lyrics = f.readlines()
         word_list = [line.split() for line in lyrics]
-        print(word_list)
+        word_list = flatten_lol(word_list)
+  print(word_list)
+
+# remove the puncuation
+  word_count = {}
+# keys be word value be freq.
+# lower the words
+  first_word = word_list[0].lower()
+  print(first_word)
+  word_count[first_word] = 1
+  print(word_count)
+  print(word_count.keys())
+
 
 # This is an "incantation." You will not see it very often, and it needs to be here to be able to pass file names as arguments.
 if __name__ == "__main__":
